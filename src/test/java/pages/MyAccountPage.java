94ff4f61a16f12cf7ba7326d69ec1706cdaa5256
Arun <arun.motoori@gmail.com>
2025-02-02 19:06:16 +0530

    Framework Batch 5 TestNG Hybrid POM PF

---
+++ src/test/java/pages/MyAccountPage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import root.RootPage;
+
+public class MyAccountPage extends RootPage {
+
+	public MyAccountPage(WebDriver driver) {
+		super(driver);
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(linkText = "Edit your account information")
+	private WebElement editYourAccountInformationOption;
+
+	@FindBy(linkText = "Subscribe / unsubscribe to newsletter")
+	private WebElement subscribeOrUnsubscribeToNewsletterOption;
+
+	public RightColumnOptions getRightColumnOptions() {
+		return new RightColumnOptions(driver);
+	}
+
+	public NewsletterSubscriptionPage clickOnSubscribeOrUnsubscribeNewsletterOption() {
+		elementUtilities.clickOnElement(subscribeOrUnsubscribeToNewsletterOption);
+		return new NewsletterSubscriptionPage(driver);
+	}
+
+	public boolean didWeNavigateToMyAccountPage() {
+		return elementUtilities.isElementDisplayed(editYourAccountInformationOption);
+	}
+
+}
